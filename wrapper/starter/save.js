a5609db89c05256da19ab00b1fceddf5988b128a
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-02-18 15:26:14 -0500

    Update save.js

--- wrapper/starter/save.js
+++ wrapper/starter/save.js
@@ -1,3 +1,4 @@
+// All rights and credits goes to David's TV Studio for helping us with the Your Library import feature.
 const loadPost = require("../misc/post_body");
 const starter = require("./main");
 const http = require("http");
